29c90559765ad4f2f6314399c4459d4ecaf350ec
Sharlene Wadhwa <sharlwadhwa@gmail.com>
2022-03-13 20:08:30 +0530

    Home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
                 <div className="row">
                     <div className="col-10 mx-auto">
                         <div className="row">
-                            <div className="col-md-6 mt-5 pt-5 pt-lg-0 order-2 order-lg-1">
+                            <div className="col-md-6 mt-lg-5 pt-3 pt-lg-0 order-1 order-lg-1">
                                 <h6 className="Marketplace my-4">
                                     Explore NFT Marketplace
                                 </h6>
@@ -28,7 +28,7 @@ function Home() {
                                     <a href=" " className="btn-get-started">GET STARTED</a>
                                 </div>
                             </div>
-                            <div className="col-lg-6 order-1 order-lg-2 header-img">
+                            <div className="col-lg-6 order-2 order-lg-2 header-img">
                                 <img src={web} className="container avatar" alt="homepage"></img>
                             </div>
                         </div>
