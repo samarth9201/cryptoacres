4f1b5a03943e5aa71e36856bbc64ce073de9803d
Samarth Bhadane <scbhadane@gmail.com>
2022-04-19 03:33:50 +0530

    Delete backend directory

--- backend/routes/BrokerRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import addBroker from "../controllers/Broker/addBroker.js";
-
-const BrokerRoutes = express.Router();
-
-BrokerRoutes.post("/add-broker", addBroker);
-
-export default BrokerRoutes;
--- backend/routes/PropertyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import addProperty from "../controllers/Property/addProperty.js";
-
-const PropertyRoutes = express.Router();
-
-PropertyRoutes.post("/add-property", addProperty);
-
-export default PropertyRoutes;
